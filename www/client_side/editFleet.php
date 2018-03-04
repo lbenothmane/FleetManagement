9d78dcf98e4ce645aeee4a20f5419e16a169e18f
Venecia Alvarez <noosh95@gmail.com>
2018-03-04 14:34:35 -0600

    Separate add and delete vehicles on edit fleet page

--- www/client_side/editFleet.php
+++ www/client_side/editFleet.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
@@ -13,7 +13,7 @@
 	<body>
 		<div ng-app="editFleet" ng-controller="editFleetCtrl">
 			<?php include("navbar.html");?>
-
+			<h2>Add New Vehicle</h2>
 			<form class="form-horizontal">
 				<div class="form-group">
 					<label class="control-label col-sm-2" for="email">Vehicle ID:</label>
@@ -21,25 +21,26 @@
 						<input type="email" class="form-control" id="email" placeholder="Vehicle ID">
 					</div>
 				</div>
-				<div class="form-group">
+				<!-- <div class="form-group">
 					<label class="control-label col-sm-2" for="pwd">Driver:</label>
 					<div class="col-sm-10">
 						<input type="password" class="form-control" id="pwd" placeholder="Driver Name">
 					</div>
-				</div>
+				</div> -->
 				<div class="form-group">
 					<div class="col-sm-offset-2 col-sm-10">
-						<div class="checkbox">
-							<label><input type="checkbox"> Remember me</label>
-						</div>
-					</div>
-				</div>
-				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-10">
-						<button type="submit" class="btn btn-default">Submit</button>
+						<button type="submit" class="btn btn-default">Add Vehicle to My Fleet</button>
 					</div>
 				</div>
 			</form>
+			<hr>
+			<h2>Delete Vehicle(s)</h2>
+			<table>
+	            <tr ng-repeat="x in vehicleIDs">
+	              <td>{{x}}</td>
+			  	  <td><button ng-click="delete(x)">Delete</button></td>
+	            </tr>
+	        </table>
 		</div>
 	</body>
 	<script src="js/services/navbarService.js"></script>
--- www/client_side/js/editFleet.js
+++ www/client_side/js/editFleet.js
@@ -1,10 +1,29 @@
 function editFleetCtrl($scope, NavbarService, SessionService){
     NavbarService.initializeNavbar($scope);
+    // $scope.username = SessionService.getCurrentUser();
+    $scope.username = "kwberner"; // TODO: Comment out for demo
+    $scope.vehicleIDs = [];
 
-    // $scope.TODO = function(){
-    //
-    // }
-}//End controller
+    // $scope.$apply($scope.getVehicleIDs);
+
+    $scope.getVehicleIDs = function(){
+        var url = "http://35.193.191.2:8080/manager/" + $scope.username;
+        $.get(url, function(data, status){
+            for(i = 0; i < data.length; i++){
+                if(data[i].uid != null){
+                    $scope.vehicleIDs.push(data[i].uid);
+                }
+            }
+            console.log($scope.vehicleIDs);
+        });
+    }
+
+    $scope.getVehicleIDs();
+
+    $scope.delete = function(vehicleID){
+        alert("Delete" + vehicleID);
+    }
+}
 
 angular
   .module('editFleet', ['NavbarService', 'SessionService'])
